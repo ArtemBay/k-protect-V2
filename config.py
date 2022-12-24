6cb44ad7aa9b4837a42a397ce16237f0e36822b4
ArtemBay <kkunfin@gmail.com>
2022-12-24 21:51:08 +0900

    Update config.py

--- config.py
+++ config.py
@@ -10,14 +10,11 @@ class Color:
 
 class Auth:
     discord_auth = {
-        "debug": "OTM2NjEzODA5ODgyMjgwMDA2.Gx2DUL.rAN5A0FXuTMEf5Tuic6SI98Nz8hSb1oZZK3y0A", #helper#0388
-        "release": "OTk5NjExMDU3ODcyMDY0NTIy.GYbUdi.WWeTqKJwGlzjjRH5m-wtKrn668itbwjac6_26o"
+        "debug": "",
+        "release": ""
     }
-    mongo_auth = "mongodb+srv://ArtemBay:YZaKp6M4IR6aYka6@main.opwrf.mongodb.net/db?retryWrites=true&w=majority"
-    qiwi_auth = "eyJ2ZXJzaW9uIjoiUDJQIiwiZGF0YSI6eyJwYXlpbl9tZXJjaGFudF9zaXRlX3VpZCI6IjFlZmliZC0wM" \
-                "CIsInVzZXJfaWQiOiI3OT" \
-                "UzMDI0NzcxMSIsInNlY3JldCI6ImNiNTYzZTJjZDMzNWFjZDMxMjQxMWFlYTYxZGMzZTdiZTE0YTI0MGEyOGMwY2M2Z" \
-                "GFhNmE4NWFlYTgyZjM3ZTQifX0="
+    mongo_auth = ""
+    qiwi_auth = ""
 
 
 class Other:
